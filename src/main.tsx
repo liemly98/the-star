a9541f5ec3c5d789920b4611271f8041b7f4e2fa
Liem Ly <liemly98@gmail.com>
2025-08-15 18:40:37 +0700

    - init project with vite and tailwind css and shadcn.

---
+++ src/main.tsx
@@ -0,0 +1,18 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { BrowserRouter, Route, Routes } from "react-router";
+import LoginPage from "./pages/login.tsx";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/login" element={<LoginPage />} />
+        {/* Add other routes here as needed */}
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
